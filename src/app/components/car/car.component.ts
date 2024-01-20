b2fff0415c6cb43b7addd199cf5717c5e4587bc7
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-01-20 17:03:59 +0300

    1/20/2024 Commit 2

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Car } from '../../models/car';
-
+import { CarResponseModel } from '../../models/carResponseModel';
+import { response } from 'express';
+import { CarService } from '../../services/car.service';
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrl: './car.component.css'
 })
-export class CarComponent {
-
- 
+export class CarComponent implements OnInit {
   cars:Car[]=[];
+  constructor(private carService:CarService){}
+  
+  ngOnInit():void {
+
+    this.getCars()
+  }
+  getCars(){
+
+    this.carService.getCars().subscribe(response =>{
+      this.cars=response.data
+    })
+  }
 }

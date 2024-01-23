97fd2768fea26691bed5cb35c50edfaca021a7ba
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-01-23 13:44:28 +0300

    1/23/2024

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -10,6 +10,7 @@ import { BrandService } from '../../services/brand.service';
 export class BrandComponent implements OnInit {
 
   brands: Brand[]=[]
+  currentBrand:Brand;
   constructor(private brandService:BrandService){}
 
   ngOnInit(): void {
@@ -22,5 +23,23 @@ export class BrandComponent implements OnInit {
       this.brands=response.data
     })
   }
+  setCurrentBrand(brand:Brand){
+    this.currentBrand=brand
+  }
+  getCurrentBrandClass(brand:Brand)
+  {
+    if(brand== this.currentBrand)
+    {
+
+      return "list-group-item active"
+    }
+    else
+    {
+
+      return "list-group-item"
+    }
+
+  }
+
 
 }
